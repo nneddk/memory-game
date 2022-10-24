ca6bae2c1bddd15e3293c01e26b3ac04bf0654af
nneddk <meljandre.mc@gmail.com>
2022-10-24 12:53:00 +0800

    skeletons updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { useEffect, useState } from "react";
 
 const App = () =>{
+  const [collectedNumber, setCollectedNumber] = useState([]);
+  const [text, setText] = useState('Change');
+  const addNumber = () =>{
+    setCollectedNumber(collectedNumber.concat('1'));
+    console.log(collectedNumber);
+
+    setText('Changed');
+  }
+  /*
   useEffect(()=>{
 
 
     return () =>{
     };
   },[]);
-
+*/
   return(
     <div>
+      <button onClick={addNumber}>{text}</button>
     </div>
   );
 };

acd3a9395fe015cf953e3fb3058fdeda112e9efb
nneddk <meljandre.mc@gmail.com>
2022-10-25 16:28:37 +0800

    adding and removal of values added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,25 +1,55 @@
 import React, { useEffect, useState } from "react";
 
 const Main = () =>{
-  const [collectedNumber, setCollectedNumber] = useState([]);
-  const [text, setText] = useState('1');
-  const addNumber = () =>{
-    setCollectedNumber(collectedNumber.concat('1'));
-    console.log(collectedNumber);
-
-    setText('1');
-  }
-  useEffect(()=>{
     
+    const [currentCards, setCurrentCards] = useState(5);
+    const [selectedCards, setSelectedCards] = useState([]);
+    //creates the memory cards
+    const createMemoryCard = (dataLimit) =>{
+        let cardData = [];
+        for(let i = 0; i<dataLimit; i++){
+            cardData.push({key:i, name:i});
+        }
+
+        cardData = shuffleArray(cardData);
+
+        const cardClick = (e) =>{
+            let value = e.target.innerHTML;
+            if (!selectedCards.includes(value)){
+                //lazy way to do this
+                setSelectedCards(selectedCards.concat(value));
+                setCurrentCards(currentCards + 1);
+                cardData = shuffleArray(cardData);
+            }
+        }
+
+        //Durstenfeld Shuffle
+        function shuffleArray(array) {
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+            return array;
+        }
+
+        const children = cardData.map((val)=>(
+            React.createElement('button', {key: val["key"], onClick: cardClick}, val['name'])
+        ));
+        
+        return children;
+      }
+      let children = createMemoryCard(currentCards);
+
+  useEffect(()=>{
+    console.log(selectedCards);
     return () =>{
       
     };
-  });
+    
+  },[currentCards, selectedCards]);
 
   return(
-    <div id = 'main'>
-      <button onClick={addNumber}>{text}</button>
-    </div>
+    React.createElement('div', {id: 'main'}, children)
   );
 };
 

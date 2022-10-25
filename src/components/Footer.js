cf40207ba30fe34c2ad53ac92bd1c2a87b51a97d
nneddk <meljandre.mc@gmail.com>
2022-10-25 12:23:47 +0800

    Footer Created

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () =>{
+    return(
+        <div id = 'footer'>
+            <a href='https://www.github.com/nneddk' targer = '_blank'>
+             @nneddk
+            </a>
+        </div>
+    );
+};
+
+export default Footer;

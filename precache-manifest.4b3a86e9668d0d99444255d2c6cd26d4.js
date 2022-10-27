958cd12cdb212ca598cde057d6d9d81a5855f2de
nneddk <meljandre.mc@gmail.com>
2022-10-27 20:11:13 +0800

    Updates

---
+++ precache-manifest.4b3a86e9668d0d99444255d2c6cd26d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bc8638728af1d271807",
+    "url": "/memory-game/static/css/main.7dc43d9a.chunk.css"
+  },
+  {
+    "revision": "3bc8638728af1d271807",
+    "url": "/memory-game/static/js/main.3bc86387.chunk.js"
+  },
+  {
+    "revision": "6f86302224b57135b89f",
+    "url": "/memory-game/static/js/1.6f863022.chunk.js"
+  },
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "b7e7a9a2f8e02d2a78bcfbb4c1edbe80",
+    "url": "/memory-game/index.html"
+  }
+];

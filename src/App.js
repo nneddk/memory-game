070da1d45e16e76ab6eb8a796aa222f6e9ac7584
nneddk <meljandre.mc@gmail.com>
2022-10-27 20:03:45 +0800

    animated background added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React  from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import AnimatedList from "./components/animatedBackground";
 import './components/styles/App.css'
 
 const App = () =>{
 
   return(
     <>
+      <AnimatedList/>
       <Header/>
       <Main/>
       <Footer/>
+      
+      
     </>
   );
 };

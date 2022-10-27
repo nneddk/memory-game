672903c19911b5bb0a6301929ee23c312794585b
nneddk <meljandre.mc@gmail.com>
2022-10-27 14:22:04 +0800

    Randomizer, win and lose states added

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,16 @@ import React, { useEffect, useState } from "react";
 const randomWords = require('random-words');
 let randomWordArray = randomWords(50);
 
+//Durstenfeld Shuffle
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    
+    return array;
+}
+
 const Main = () =>{
     const [currentCards, setCurrentCards] = useState(5);
     const [selectedCards, setSelectedCards] = useState([]);
@@ -16,26 +26,24 @@ const Main = () =>{
             cardData.push({key:i, name:randomWordArray[i]});
         }
         
-        //Durstenfeld Shuffle
-        function shuffleArray(array) {
-            for (let i = array.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [array[i], array[j]] = [array[j], array[i]];
-            }
-            return array;
-        }
+        
         cardData= shuffleArray(cardData);
         
 
         const cardClick = (e) =>{
             let value = e.target.innerHTML;
+            
             if (!selectedCards.includes(value)){
+                if(score === 10){
+                    console.log('win state');
+                }
                 setScore(score + 1);
                 //lazy way to do this
                 setSelectedCards(selectedCards.concat(value));
                 setCurrentCards(currentCards + 1);
                 cardData = shuffleArray(cardData);
             }else{
+                console.log('lose state');
                 startGame();
             }
         }
@@ -48,23 +56,29 @@ const Main = () =>{
 
       const startGame = () =>{
         setIsGameReady(!isGameReady);
-        console.log(isGameReady);
       }
 
-      const startGameBtn = React.createElement('button', {className: 'start-game-btn', onClick: startGame, type: 'button'}, 'Start Game');
-      let children = isGameReady?createMemoryCard(currentCards):startGameBtn;
+      let startGameDesc = 'test';
+
+      const currentScore = React.createElement('div', {key: 'score', className: 'current-score'}, score);
+      const startGameInstructions = React.createElement('div', {key: 'startGameDesc', className: 'start-game-desc'}, startGameDesc);
+      const startGameBtn = React.createElement('button', {key: 'startGameBtn', className: 'start-game-btn', onClick: startGame, type: 'button'}, 'Start Game');
+     
+      let children = isGameReady?[currentScore, createMemoryCard(currentCards)]:[startGameInstructions, startGameBtn];
     
   useEffect(()=>{
-    console.log('render');
+    
     return () =>{
-      
+        console.log('render');
+        setCurrentCards(5);
+        setSelectedCards([]);
+        setScore(0);
+        randomWordArray = shuffleArray(randomWordArray);
     };
     
   },[isGameReady]);
   return(
-    <div>
-        <div id = 'score'>{score}</div>
-
+    <div>   
         {React.createElement('div', {id: 'main'},children)}
     </div>
     
